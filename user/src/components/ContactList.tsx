9d6ce95187b6b3669df945bc7b471dee8d84b213
amalshaaji <amalshajiamal197970@gmail.com>
2025-01-01 23:31:20 +0530

    ContactList Updated

--- user/src/components/ContactList.tsx
+++ user/src/components/ContactList.tsx
@@ -4,9 +4,35 @@ function ChatApp() {
   return (
     <div className=" text-black w-[25%] h-full p-4 overflow-y-scroll" style={{ backgroundColor: "#E3E1F4" }}>
       {/* Contact 1 */}
+
+      <div className="flex items-center justify-between py-3">
+  <img
+    src="https://png.pngtree.com/png-clipart/20200224/original/pngtree-cartoon-color-simple-male-avatar-png-image_5230557.jpg"
+    alt="Fabindia Limited avatar"
+    className="w-12 h-12 rounded-full object-cover"
+  />
+  <div className="flex-1 mx-4">
+    <div className="flex justify-between">
+      <h3 className="text-md font-medium">User Name</h3>
+      <div className="flex flex-col items-end">
+        <span className="text-xs text-gray-500">18:03</span>
+        <span
+          className="w-5 h-5 mt-1 flex items-center justify-center rounded-full text-sm font-semibold"
+          style={{ backgroundColor: "#3B82F6", color: "white" }}
+        >
+          1
+        </span>
+      </div>
+    </div>
+    <p className="text-sm text-gray-600 truncate">hello...</p>
+  </div>
+</div>
+
+
+
       <div className="flex items-center justify-between py-3 border-gray-300">
         <img
-          src="https://via.placeholder.com/50"
+          src="https://png.pngtree.com/png-clipart/20200224/original/pngtree-cartoon-color-simple-male-avatar-png-image_5230557.jpg"
           alt="Fabindia Limited avatar"
           className="w-12 h-12 rounded-full object-cover"
         />
@@ -22,7 +48,7 @@ function ChatApp() {
 
       <div className="flex items-center justify-between py-3 border-gray-300">
         <img
-          src="https://via.placeholder.com/50"
+          src="https://png.pngtree.com/png-clipart/20200224/original/pngtree-cartoon-color-simple-male-avatar-png-image_5230557.jpg"
           alt="Fabindia Limited avatar"
           className="w-12 h-12 rounded-full object-cover"
         />
@@ -38,7 +64,7 @@ function ChatApp() {
 
       <div className="flex items-center justify-between py-3 border-gray-300">
         <img
-          src="https://via.placeholder.com/50"
+          src="https://png.pngtree.com/png-clipart/20200224/original/pngtree-cartoon-color-simple-male-avatar-png-image_5230557.jpg"
           alt="Fabindia Limited avatar"
           className="w-12 h-12 rounded-full object-cover"
         />
@@ -54,7 +80,7 @@ function ChatApp() {
 
       <div className="flex items-center justify-between py-3 border-gray-300">
         <img
-          src="https://via.placeholder.com/50"
+          src="https://png.pngtree.com/png-clipart/20200224/original/pngtree-cartoon-color-simple-male-avatar-png-image_5230557.jpg"
           alt="Fabindia Limited avatar"
           className="w-12 h-12 rounded-full object-cover"
         />
@@ -70,7 +96,7 @@ function ChatApp() {
 
       <div className="flex items-center justify-between py-3 border-gray-300">
         <img
-          src="https://via.placeholder.com/50"
+          src="https://png.pngtree.com/png-clipart/20200224/original/pngtree-cartoon-color-simple-male-avatar-png-image_5230557.jpg"
           alt="Fabindia Limited avatar"
           className="w-12 h-12 rounded-full object-cover"
         />
@@ -86,7 +112,7 @@ function ChatApp() {
 
       <div className="flex items-center justify-between py-3 border-gray-300">
         <img
-          src="https://via.placeholder.com/50"
+          src="https://png.pngtree.com/png-clipart/20200224/original/pngtree-cartoon-color-simple-male-avatar-png-image_5230557.jpg"
           alt="Fabindia Limited avatar"
           className="w-12 h-12 rounded-full object-cover"
         />
@@ -102,7 +128,7 @@ function ChatApp() {
 
       <div className="flex items-center justify-between py-3 border-gray-300">
         <img
-          src="https://via.placeholder.com/50"
+          src="https://png.pngtree.com/png-clipart/20200224/original/pngtree-cartoon-color-simple-male-avatar-png-image_5230557.jpg"
           alt="Fabindia Limited avatar"
           className="w-12 h-12 rounded-full object-cover"
         />
@@ -118,7 +144,7 @@ function ChatApp() {
 
       <div className="flex items-center justify-between py-3 border-gray-300">
         <img
-          src="https://via.placeholder.com/50"
+          src="https://png.pngtree.com/png-clipart/20200224/original/pngtree-cartoon-color-simple-male-avatar-png-image_5230557.jpg"
           alt="Fabindia Limited avatar"
           className="w-12 h-12 rounded-full object-cover"
         />
@@ -134,7 +160,7 @@ function ChatApp() {
 
       <div className="flex items-center justify-between py-3 border-gray-300">
         <img
-          src="https://via.placeholder.com/50"
+          src="https://png.pngtree.com/png-clipart/20200224/original/pngtree-cartoon-color-simple-male-avatar-png-image_5230557.jpg"
           alt="Fabindia Limited avatar"
           className="w-12 h-12 rounded-full object-cover"
         />
@@ -150,7 +176,7 @@ function ChatApp() {
 
       <div className="flex items-center justify-between py-3 border-gray-300">
         <img
-          src="https://via.placeholder.com/50"
+          src="https://png.pngtree.com/png-clipart/20200224/original/pngtree-cartoon-color-simple-male-avatar-png-image_5230557.jpg"
           alt="Fabindia Limited avatar"
           className="w-12 h-12 rounded-full object-cover"
         />
@@ -166,23 +192,7 @@ function ChatApp() {
 
       <div className="flex items-center justify-between py-3 border-gray-300">
         <img
-          src="https://via.placeholder.com/50"
-          alt="Fabindia Limited avatar"
-          className="w-12 h-12 rounded-full object-cover"
-        />
-        <div className="flex-1 mx-4">
-          <div className="flex justify-between">
-            <h3 className="text-md font-medium">User Name</h3>
-            <span className="text-xs text-gray-500">18:03</span>
-          </div>
-          <p className="text-sm text-gray-600 truncate">hello...</p>
-        </div>
-      </div>
-
-
-      <div className="flex items-center justify-between py-3 border-gray-300">
-        <img
-          src="https://via.placeholder.com/50"
+          src="https://png.pngtree.com/png-clipart/20200224/original/pngtree-cartoon-color-simple-male-avatar-png-image_5230557.jpg"
           alt="Fabindia Limited avatar"
           className="w-12 h-12 rounded-full object-cover"
         />
