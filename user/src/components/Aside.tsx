e60b91768d71176c5d593595e826a847cda55d61
Alwinkunjachan <alwinpkunjachan@gmail.com>
2025-01-02 20:19:15 +0530

    clerk changes

--- user/src/components/Aside.tsx
+++ user/src/components/Aside.tsx
@@ -1,56 +1,70 @@
 // import { cn } from "@/lib/utils";
-import { ModeToggle } from "./mode-toggle"
-import { Settings } from 'lucide-react';
-import { Menu } from 'lucide-react';
+import { ModeToggle } from "./mode-toggle";
+import { Settings } from "lucide-react";
+import { Menu } from "lucide-react";
 import { useState } from "react";
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
+import { UserButton } from "@clerk/clerk-react";
 
 const Aside = () => {
-    const [isAsideOpen, setIsAsideOpen] = useState(false);
+  const [isAsideOpen, setIsAsideOpen] = useState(false);
 
-    const handleAsideOpen = () => {
-        setIsAsideOpen(!isAsideOpen);
-    }
+  const handleAsideOpen = () => {
+    setIsAsideOpen(!isAsideOpen);
+  };
 
-    return (
-        <motion.div 
-        className="aside-bg flex flex-col h-full"
-        animate={{ width: isAsideOpen ? '170px' : '90px' }}
-        transition={{ duration: 0.3, ease: 'easeInOut' }}
+  return (
+    <motion.div
+      className="aside-bg flex flex-col h-full"
+      animate={{ width: isAsideOpen ? "170px" : "90px" }}
+      transition={{ duration: 0.3, ease: "easeInOut" }}
     >
-        <div className="h-1/2">
-            <h2 className="text-white font-semibold text-center pt-2">Emotract</h2>
-        </div>
-        <div className="h-1/2 flex flex-col justify-end items-start pb-10 gap-7 relative">
-            <span className="flex items-center w-full pl-7 gap-2">
-                <Settings color="white" className="w-8 h-8" cursor="pointer"/>
-                <motion.p 
-                    className="text-white font-semibold text-[1.2rem] overflow-hidden"
-                    initial={{ opacity: 0, width: 0 }}
-                    animate={{ opacity: isAsideOpen ? 1 : 0, width: isAsideOpen ? 'auto' : 0 }}
-                    transition={{ duration: 0.3, ease: 'easeInOut' }}
-                >
-                    Settings
-                </motion.p>
-            </span>
-            <span className="flex items-center w-full pl-7 gap-2">
-                <Menu onClick={handleAsideOpen} color="white" className="w-8 h-8" cursor="pointer"/>
-                <motion.p 
-                    className="text-white font-semibold text-[1.2rem] overflow-hidden"
-                    initial={{ opacity: 0, width: 0 }}
-                    animate={{ opacity: isAsideOpen ? 1 : 0, width: isAsideOpen ? 'auto' : 0 }}
-                    transition={{ duration: 0.3, ease: 'easeInOut' }}
-                >
-                    Menu
-                </motion.p>
-            </span>
-            <span className="flex items-center w-full pl-6">
-                <ModeToggle />
-            </span>
-        </div>
+      <div className="h-1/2">
+        <h2 className="text-white font-semibold text-center pt-2">Emotract</h2>
+      </div>
+      <div className="h-1/2">
+        <UserButton />
+      </div>
+      <div className="h-1/2 flex flex-col justify-end items-start pb-10 gap-7 relative">
+        <span className="flex items-center w-full pl-7 gap-2">
+          <Settings color="white" className="w-8 h-8" cursor="pointer" />
+          <motion.p
+            className="text-white font-semibold text-[1.2rem] overflow-hidden"
+            initial={{ opacity: 0, width: 0 }}
+            animate={{
+              opacity: isAsideOpen ? 1 : 0,
+              width: isAsideOpen ? "auto" : 0,
+            }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+          >
+            Settings
+          </motion.p>
+        </span>
+        <span className="flex items-center w-full pl-7 gap-2">
+          <Menu
+            onClick={handleAsideOpen}
+            color="white"
+            className="w-8 h-8"
+            cursor="pointer"
+          />
+          <motion.p
+            className="text-white font-semibold text-[1.2rem] overflow-hidden"
+            initial={{ opacity: 0, width: 0 }}
+            animate={{
+              opacity: isAsideOpen ? 1 : 0,
+              width: isAsideOpen ? "auto" : 0,
+            }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+          >
+            Menu
+          </motion.p>
+        </span>
+        <span className="flex items-center w-full pl-6">
+          <ModeToggle />
+        </span>
+      </div>
     </motion.div>
-    
-    )
-}
+  );
+};
 
-export default Aside
+export default Aside;
--- user/src/main.tsx
+++ user/src/main.tsx
@@ -1,18 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { ThemeProvider } from "@/components/theme-provider"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
+import App from "./App.tsx";
+import { ClerkProvider } from "@clerk/clerk-react";
+const { VITE_CLERK_PUBLISHABLE_KEY } = import.meta.env;
+const publishableKey = VITE_CLERK_PUBLISHABLE_KEY;
 
-import App from './App.tsx'
+if (!publishableKey) {
+  throw new Error("Key was not found");
+}
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider
-        defaultTheme="dark"
-        storageKey="emotract-user-theme"
-      >
-      <App />
+    <ThemeProvider defaultTheme="dark" storageKey="emotract-user-theme">
+      <ClerkProvider publishableKey={publishableKey} afterSignOutUrl="/">
+        <App />
+      </ClerkProvider>
     </ThemeProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
